3862e3c7d3adefd39adc4a8c4aa93ee591d320c5
HARISREE SURESH <getharisree@outlook.com>
2023-01-13 14:27:04 +0530

    Update creating_and_linking_database.py

--- creating_and_linking_database.py
+++ creating_and_linking_database.py
@@ -1,1 +1,19 @@
-
+ try:
+                file = open('datasheet.txt','r+')
+                d = file.read()
+                r = ast.literal_eval(d)
+                
+                dict2 = {userrname:passsword}
+                r.update(dict2)
+                file.truncate(0)
+                file.close()
+                
+                file=open('datasheet.txt','w')
+                w = file.write(str(r))
+                
+                messagebox.showinfo(message = " signed up succesfully")
+  except:
+                file=open('datasheet.txt','w')
+                pp = str({'username':'password'})
+                file.write(pp)
+                file.close()
